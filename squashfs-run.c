27006c52d4458df00bf4e7e023b00256a49c47cd
Harmen Stoppels <harmenstoppels@gmail.com>
2022-07-08 13:04:06 +0200

    set version to 0.2.3-dev

--- squashfs-run.c
+++ squashfs-run.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <unistd.h>
 
-#define VERSION "0.2.2"
+#define VERSION "0.2.3-dev"
 #define HEADER_SIZE 4096
 
 #define exit_with_error(str)                                                   \
